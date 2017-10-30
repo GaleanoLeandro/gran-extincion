947163b9451a75793dd989ee2a9a76014e39d5b7
Leandro Galeano <leandrogaleano96@hotmail.com>
2017-10-30 11:06:03 -0300

    seccion 'inicio' funcionamiento basico

--- js/inicio.js
+++ js/inicio.js
@@ -31,15 +31,78 @@ AFRAME.registerComponent('inicio', {
         this.textBox.setAttribute( 'color', '#818184')
         this.textBox.setAttribute( 'width', 3.9)
         this.textBox.setAttribute( 'height', 1.1)
+        this.textAnimate(this.textBox, this.showBox, this.showBox);
         
 
         //Crea texto 1 luis
-        this.createImage('texto-1', 'texto-1', '#texto-1', -1, .8, -2.3, 3.9, 1.1)
+        this.createImage('texto-1', 'texto-1', '#texto-1', -1, .8, -2.2, 3.9, 0)
+        this.texto1 = document.querySelector('.texto-1')
+        this.textAnimate(this.texto1, this.showText1, this.showText1);
+
+        //Crea texto 2 luis
+        this.createImage('texto-2', 'texto-2', '#texto-2', -1, .8, -2.2, 3.9, 0)
+        this.texto2 = document.querySelector('.texto-2')
+        this.textAnimate(this.texto2, this.showText2, this.showText2);
+        
+        //Crea texto 3 luis
+        this.createImage('texto-3', 'texto-3', '#texto-3', -1, .8, -2.2, 3.9, 0)
+        this.texto3 = document.querySelector('.texto-3')
+        this.textAnimate(this.texto3, this.showText3, this.showText3);
+
+        //funcion mostrar texto
+        var textInit = 1000
+            textInterval = 3000,
+            timeText = textInit + textInterval;
+
+        setTimeout(() => {
+            this.texto1.emit('mostrar')
+        }, textInit);
+        setTimeout( () => {
+            this.texto1.emit('ocultar')
+            this.texto2.emit('mostrar')
+        }, timeText);
+        setTimeout(() => {
+            this.texto2.emit('ocultar')
+            this.texto3.emit('mostrar')
+        }, timeText + textInterval);
+        setTimeout(() => {
+            this.textBox.emit('ocultar')
+            this.texto3.emit('ocultar')
+        }, timeText + textInterval * 2);
+
+        //remover textos del DOM para optimizar rendimiento
+        this.textBox.addEventListener('animationend', () => {
+            this.el.removeChild(this.texto1)
+            this.el.removeChild(this.texto2)
+            this.el.removeChild(this.texto3)
+        })
 
         // funcion para detectar click en el portal
         this.portal.addEventListener('click' , () => {
             escenaEl.components['escena'].estado(nextState)
         })
+    },
+    textAnimate (parentEl, nameAnimEnter, nameAnimLeave ) {
+        //parentEl = elemento que recibe la animacion
+        //nameAnimEnter = nombre de la animacion para show
+        //nameAnimLeave = nombre de la animacion para hidde
+
+        //animacion mostrar texto
+        nameAnimEnter = document.createElement('a-animation')
+        nameAnimEnter.setAttribute('attribute', 'height')
+        nameAnimEnter.setAttribute('from', 0 )
+        nameAnimEnter.setAttribute('to', 1.1 )
+        nameAnimEnter.setAttribute('begin', 'mostrar')
+
+        //animacion ocultar texto
+        nameAnimLeave = document.createElement('a-animation')
+        nameAnimLeave.setAttribute('attribute', 'height')
+        nameAnimLeave.setAttribute('from', 1.1 )
+        nameAnimLeave.setAttribute('to', 0 )
+        nameAnimLeave.setAttribute('begin', 'ocultar')
+
+        parentEl.appendChild(nameAnimEnter)
+        parentEl.appendChild(nameAnimLeave)
     },
     createImage (nameEl, className, idSrc, x, y, z, w, h) {
         var pos = { x: x, y: y, z: z }
