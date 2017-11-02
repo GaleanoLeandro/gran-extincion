729a49f84f63257551d328f8c68d3d355999304c
Leandro Galeano <leandrogaleano96@hotmail.com>
2017-11-02 09:10:19 -0300

    Commit 0 rama meteorito, cambio de invel funcionando

--- js/escena.js
+++ js/escena.js
@@ -25,15 +25,14 @@ AFRAME.registerComponent('escena', {
             //Remover todos los elementos de otros escenarios.
             this.el.removeChild(this.escena2)
             
-            //crear componente del escenario actual
+            //Crear componente del escenario actual
             this.escena1 = document.createElement('a-entity')
             
-            // agrega componente a la escena
+            //Agrega componente a la escena
             this.el.appendChild(this.escena1)
             
             this.escena1.setAttribute('inicio', '')
-        }
-        if (data == 2) {
+        } else if (data == 2) {
             this.el.removeChild(this.escena1)
 
             this.escena2 = document.createElement('a-entity')
@@ -41,6 +40,14 @@ AFRAME.registerComponent('escena', {
             this.el.appendChild(this.escena2)
           
             this.escena2.setAttribute('mesozoico', '')
+        } else if (data == 3) {
+            this.el.removeChild(this.escena2)
+
+            this.escena3 = document.createElement('a-entity')
+
+            this.el.appendChild(this.escena3)
+
+            this.escena3.setAttribute('meteorito', '')
         }
     },
     remove: function () {
